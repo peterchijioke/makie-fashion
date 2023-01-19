a69a8dc25a715bcf858554cbe662d59147719524
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-01-19 16:41:29 +0100

    u

---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
+export default function index() {
+  return (
+    <div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <ProtectedPage />
+              </RequireAuth>
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
