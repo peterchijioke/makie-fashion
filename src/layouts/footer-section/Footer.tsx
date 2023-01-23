1e8835ed57599a0ec343c184bb07468441eb4fba
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-01-23 15:16:00 +0100

    update

--- src/layouts/footer-section/Footer.tsx
+++ src/layouts/footer-section/Footer.tsx
@@ -1,11 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.scss";
+import TwitterSVG from "../../images/twitter.svg";
 
 export default function Footer() {
   return (
     <div className="footer-wrapper">
       <div className="footer-main">
-        <div className="footer-main-inner one">peter</div>
+        <div className="footer-main-inner one">
+          <label htmlFor="social">Our Resources</label>
+          <img
+            src={TwitterSVG}
+            style={{ height: 45, width: 45, color: "white" }}
+          />
+        </div>
         <div className="footer-main-inner two">paul</div>
       </div>
       <div className="copy-write">
