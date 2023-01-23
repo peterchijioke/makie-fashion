57589a850dd4b9c6f0e63491c668de1785cf50bd
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-01-23 14:42:50 +0100

    update

--- src/layouts/footer-section/Footer.tsx
+++ src/layouts/footer-section/Footer.tsx
@@ -4,7 +4,10 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="footer-wrapper">
-      <div className="footer-main"></div>
+      <div className="footer-main">
+        <div className="footer-main-inner one">peter</div>
+        <div className="footer-main-inner two">paul</div>
+      </div>
       <div className="copy-write">
         &#169;{new Date().getFullYear()} Makie.store
       </div>
