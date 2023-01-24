d24955860fc82cecc2a34332748b3549561441c2
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-01-24 13:35:12 +0100

    update

--- src/layouts/category-card/CategoryCard.tsx
+++ src/layouts/category-card/CategoryCard.tsx
@@ -2,9 +2,33 @@ import React, { CSSProperties } from "react";
 import "./CategorySection.scss";
 import WatchImage from "../../images/wat.png";
 import PhoneImage from "../../images/phone.png";
+import PlusIcon from "../../images/plus.svg";
 export default function CategoryCard() {
   return (
     <div className="banner-side one_">
+      {Array(4)
+        .fill(0)
+        .map((_, index) => (
+          <div className="item-in-grid">
+            {index === 0 ? (
+              <div className="add-btn">
+                <label htmlFor="Got Stuff?"> Got Stuff ?</label>
+                <img className="image-plus" src={PlusIcon} />
+              </div>
+            ) : (
+              <img
+                style={{ width: 100, height: 100 }}
+                className="image-cat"
+                src={
+                  index !== 0 && index !== 2 && index !== 5 && index !== 3
+                    ? WatchImage
+                    : PhoneImage
+                }
+              />
+            )}
+          </div>
+        ))}
+
       {Array(10)
         .fill(0)
         .map((_, index) => (
@@ -14,6 +38,7 @@ export default function CategoryCard() {
           >
             <div className="inner-row-item">
               <img
+                // style={{ width: 45, height: 45 }}
                 className="image-cat"
                 src={
                   index !== 0 && index !== 2 && index !== 5 && index !== 3
--- src/layouts/footer-section/Footer.tsx
+++ src/layouts/footer-section/Footer.tsx
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.scss";
 import TwitterSVG from "../../images/twitter.svg";
+import FacebookSVG from "../../images/facebook.svg";
+import InstagramSVG from "../../images/instagram.svg";
 
 export default function Footer() {
   return (
@@ -9,12 +11,26 @@ export default function Footer() {
       <div className="footer-main">
         <div className="footer-main-inner one">
           <label htmlFor="social">Our Resources</label>
-          <img
-            src={TwitterSVG}
-            style={{ height: 45, width: 45, color: "white" }}
-          />
+          <div className="icon-row">
+            {Array(3)
+              .fill(0)
+              .map((_, i) => (
+                <div style={{ marginLeft: i === 0 ? 0 : 10 }}>
+                  <img
+                    className="icon"
+                    src={
+                      i === 0
+                        ? TwitterSVG
+                        : i === 1
+                        ? FacebookSVG
+                        : InstagramSVG
+                    }
+                  />
+                </div>
+              ))}
+          </div>
         </div>
-        <div className="footer-main-inner two">paul</div>
+        <div className="footer-main-inner two">{/* To do */}</div>
       </div>
       <div className="copy-write">
         &#169;{new Date().getFullYear()} Makie.store
