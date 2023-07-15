f4c4cf9e8ab70447d40ad408f22f3544c4246dd7
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-07-15 12:23:37 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Routes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
